15ca387065f2439148ca5aff25867ceae0b9dc76
Maksym Pugach <pugach.m@gmail.com>
2018-07-11 18:26:15 +0300

    add precompiled version

---
+++ 1.10.0-precomiled/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+if [[ $(echo "$1" | cut -c1) = "-" ]]; then
+  echo "$0: assuming arguments for dogecoind"
+
+  set -- dogecoind "$@"
+fi
+
+if [[ $(echo "$1" | cut -c1) = "-" ]] || [[ "$1" = "dogecoind" ]]; then
+  mkdir -p "$BITCOIN_DATA"
+  chmod 700 "$BITCOIN_DATA"
+
+  echo "$0: setting data directory to $BITCOIN_DATA"
+
+  cp $BITCOIN_ROOT/dogecoin.conf $BITCOIN_DATA
+
+  set -- "$@" -datadir="$BITCOIN_DATA"
+fi
+
+if [[ "$1" = "dogecoind" ]] || [[ "$1" = "dogecoin-cli" ]] || [[ "$1" = "dogecoin-tx" ]]; then
+  echo
+  exec "$@"
+fi
+
+echo
+exec "$@"
